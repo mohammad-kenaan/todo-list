f02477de1ce2da5158b6c2679afedd37ab559658
Mohammad Kenaan <177649063+mohammad-kenaan@users.noreply.github.com>
2025-10-20 11:09:00 -0400

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.html$/i, loader: "html-loader" },
+      { test: /\.(webp|png|jpg|gif|jpeg|svg)$/i, type: "asset/resource" },
+      { test: /\.(woff|woff2)$/i, type: "asset/resource" },
+    ]
+  },
+
+}
---
+++ webpack.dev.js
@@ -0,0 +1,21 @@
+
+
+
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+
+module.exports = 
+
+  merge(
+    common, 
+    {
+      mode: "development",
+      devtool: "eval-source-map",
+      devServer: {
+          watchFiles: ["./src/*.html"],
+          hot: true,
+          static: './dist',
+      }
+    }
+  )
